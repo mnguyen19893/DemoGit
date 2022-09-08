5a299655907486232c429a0e6cc80d94d8deae27
Minh Hoang Nguyen <mnguyen83@academic.rrc.ca>
2022-09-08 17:08:49 -0500

    Initiate the project.

---
+++ app/src/main/java/com/minh/myfirstapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.minh.myfirstapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+// Define keys for intent extras with your app's package name as a prefix.
+// This ensures that the keys are unique, in case your app interacts with other apps.
+const val EXTRA_MESSAGE = "com.minh.myfirstapplication.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
+    }
+
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
